178eee60c4660eaa5cbebcfeb9efbec12efdc3e8
Will Neal <willwneal@gmail.com>
2022-02-01 16:53:13 -0500

    Added dates using moment JS and adjust JS so that forecast is five days into the future instead of including the current day again

--- assets/script.js
+++ assets/script.js
@@ -66,36 +66,36 @@ function getForecast(){
             console.log("this is the one call data below")
             console.log(oneCallData)
             citySun.textContent = "UV Index: " + oneCallData.current.uvi;
-            forecastDate1.textContent = oneCallData.daily[0].dt;
-            forecastIcon1.setAttribute("src", "https://openweathermap.org/img/wn/" + oneCallData.daily[0].weather[0].icon + "@2x.png");
-            forecastIcon1.setAttribute("alt", oneCallData.daily[0].weather[0].description)
-            forecastTemp1.textContent = "Temp: " + oneCallData.daily[0].temp.day;
-            forecastWind1.textContent = "Wind: " + oneCallData.daily[0].wind_speed;
-            forecastHumidity1.textContent = "Humidity: " + oneCallData.daily[0].humidity;
-            forecastDate2.textContent = oneCallData.daily[1].dt;
-            forecastIcon2.setAttribute("src", "https://openweathermap.org/img/wn/" + oneCallData.daily[1].weather[0].icon + "@2x.png");
-            forecastIcon2.setAttribute("alt", oneCallData.daily[1].weather[0].description)
-            forecastTemp2.textContent = "Temp: " + oneCallData.daily[1].temp.day;
-            forecastWind2.textContent = "Wind: " + oneCallData.daily[1].wind_speed;
-            forecastHumidity2.textContent = "Humidity: " + oneCallData.daily[1].humidity;
-            forecastDate3.textContent = oneCallData.daily[2].dt;
-            forecastIcon3.setAttribute("src", "https://openweathermap.org/img/wn/" + oneCallData.daily[2].weather[0].icon + "@2x.png");
-            forecastIcon3.setAttribute("alt", oneCallData.daily[2].weather[0].description)
-            forecastTemp3.textContent = "Temp: " + oneCallData.daily[2].temp.day;
-            forecastWind3.textContent = "Wind: " + oneCallData.daily[2].wind_speed;
-            forecastHumidity3.textContent = "Humidity: " + oneCallData.daily[2].humidity;
-            forecastDate4.textContent = oneCallData.daily[3].dt;
-            forecastIcon4.setAttribute("src", "https://openweathermap.org/img/wn/" + oneCallData.daily[3].weather[0].icon + "@2x.png");
-            forecastIcon4.setAttribute("alt", oneCallData.daily[3].weather[0].description)
-            forecastTemp4.textContent = "Temp: " + oneCallData.daily[3].temp.day;
-            forecastWind4.textContent = "Wind: " + oneCallData.daily[3].wind_speed;
-            forecastHumidity4.textContent = "Humidity: " + oneCallData.daily[3].humidity;
-            forecastDate5.textContent = oneCallData.daily[4].dt;
-            forecastIcon5.setAttribute("src", "https://openweathermap.org/img/wn/" + oneCallData.daily[4].weather[0].icon + "@2x.png");
-            forecastIcon5.setAttribute("alt", oneCallData.daily[4].weather[0].description)
-            forecastTemp5.textContent = "Temp: " + oneCallData.daily[4].temp.day;
-            forecastWind5.textContent = "Wind: " + oneCallData.daily[4].wind_speed;
-            forecastHumidity5.textContent = "Humidity: " + oneCallData.daily[4].humidity;
+            forecastDate1.textContent = moment().add(1, 'days').format("L");
+            forecastIcon1.setAttribute("src", "https://openweathermap.org/img/wn/" + oneCallData.daily[1].weather[0].icon + "@2x.png");
+            forecastIcon1.setAttribute("alt", oneCallData.daily[1].weather[0].description)
+            forecastTemp1.textContent = "Temp: " + oneCallData.daily[1].temp.day;
+            forecastWind1.textContent = "Wind: " + oneCallData.daily[1].wind_speed;
+            forecastHumidity1.textContent = "Humidity: " + oneCallData.daily[1].humidity;
+            forecastDate2.textContent = moment().add(2, 'days').format("L");
+            forecastIcon2.setAttribute("src", "https://openweathermap.org/img/wn/" + oneCallData.daily[2].weather[0].icon + "@2x.png");
+            forecastIcon2.setAttribute("alt", oneCallData.daily[2].weather[0].description)
+            forecastTemp2.textContent = "Temp: " + oneCallData.daily[2].temp.day;
+            forecastWind2.textContent = "Wind: " + oneCallData.daily[2].wind_speed;
+            forecastHumidity2.textContent = "Humidity: " + oneCallData.daily[2].humidity;
+            forecastDate3.textContent = moment().add(3, 'days').format("L");
+            forecastIcon3.setAttribute("src", "https://openweathermap.org/img/wn/" + oneCallData.daily[3].weather[0].icon + "@2x.png");
+            forecastIcon3.setAttribute("alt", oneCallData.daily[3].weather[0].description)
+            forecastTemp3.textContent = "Temp: " + oneCallData.daily[3].temp.day;
+            forecastWind3.textContent = "Wind: " + oneCallData.daily[3].wind_speed;
+            forecastHumidity3.textContent = "Humidity: " + oneCallData.daily[3].humidity;
+            forecastDate4.textContent = moment().add(4, 'days').format("L");
+            forecastIcon4.setAttribute("src", "https://openweathermap.org/img/wn/" + oneCallData.daily[4].weather[0].icon + "@2x.png");
+            forecastIcon4.setAttribute("alt", oneCallData.daily[4].weather[0].description)
+            forecastTemp4.textContent = "Temp: " + oneCallData.daily[4].temp.day;
+            forecastWind4.textContent = "Wind: " + oneCallData.daily[4].wind_speed;
+            forecastHumidity4.textContent = "Humidity: " + oneCallData.daily[4].humidity;
+            forecastDate5.textContent = moment().add(5, 'days').format("L");
+            forecastIcon5.setAttribute("src", "https://openweathermap.org/img/wn/" + oneCallData.daily[5].weather[0].icon + "@2x.png");
+            forecastIcon5.setAttribute("alt", oneCallData.daily[5].weather[0].description)
+            forecastTemp5.textContent = "Temp: " + oneCallData.daily[5].temp.day;
+            forecastWind5.textContent = "Wind: " + oneCallData.daily[5].wind_speed;
+            forecastHumidity5.textContent = "Humidity: " + oneCallData.daily[5].humidity;
             })
         })
     })
