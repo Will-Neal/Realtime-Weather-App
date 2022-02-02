ca3c5af4b4b8459f0483a25f8a42b02e91d1891d
Will Neal <willwneal@gmail.com>
2022-02-02 09:53:51 -0500

    Added conditional checking for response status from server so that an alert will fire alerting the user of a 404 and it wont run the app with no data

--- assets/script.js
+++ assets/script.js
@@ -56,78 +56,85 @@ function getForecast(cityName){
     // console.log(cityName)
     fetch(apiURL + apiKey)
     .then(function(response){
-        return response.json()
-    })
-    .then(function(data){
-        // console.log(data)
-        // console.log(data.list[0].main)
-        // console.log(data.city.name)
-        var weatherContainer = document.querySelector("#weatherContainer")
-        weatherContainer.classList.remove("hidden");
-        cityHead.textContent = data.city.name + " " + "(" + moment().format("L") + ")";
-        cityTemp.textContent = "Temperature: " + data.list[0].main.temp;
-        cityMoist.textContent = "Humidity: " + data.list[0].main.humidity + "%";
-        cityWind.textContent = "Wind Speed: " + data.list[0].wind.speed + "MPH";
-        citySun.textContent = "UV Index: ";
-        
-        
-        var lat = data.city.coord.lat
-        var lon = data.city.coord.lon
-        var oneCallURL = "https://api.openweathermap.org/data/2.5/onecall?lat="+ lat + "&lon=" + lon + "&units=imperial&appid=" + apiKey;
-        fetch(oneCallURL)
-        .then(function(secondResponse){
-            return secondResponse.json()
-        .then(function(oneCallData){
-            // console.log("this is the one call data below")
-            console.log(oneCallData)
-            citySun.textContent = "UV Index: " + oneCallData.daily[0].uvi;
-            uvIndexValue = Number(oneCallData.daily[0].uvi)
-            console.log("uvIndexValue: " +uvIndexValue)
-            if (uvIndexValue <= 3) {
-                // console.log("the Uv Index is low")
-                citySun.style.backgroundColor = "green";
-            } else if (uvIndexValue >= 7) {
-                // console.log("the UV Index is severe")
-                citySun.style.backgroundColor = "red";
-            } else {
-                // console.log("the UV Index is moderate")
-                citySun.style.backgroundColor = "yellow"
-            }
-            cityIcon.setAttribute("src", "https://openweathermap.org/img/wn/" + oneCallData.daily[0].weather[0].icon + "@2x.png");
-            cityIcon.setAttribute("alt", oneCallData.daily[0].weather[0].description);
-            forecastDate1.textContent = moment().add(1, 'days').format("L");
-            forecastIcon1.setAttribute("src", "https://openweathermap.org/img/wn/" + oneCallData.daily[1].weather[0].icon + "@2x.png");
-            forecastIcon1.setAttribute("alt", oneCallData.daily[1].weather[0].description)
-            forecastTemp1.textContent = "Temp: " + oneCallData.daily[1].temp.day;
-            forecastWind1.textContent = "Wind: " + oneCallData.daily[1].wind_speed;
-            forecastHumidity1.textContent = "Humidity: " + oneCallData.daily[1].humidity;
-            forecastDate2.textContent = moment().add(2, 'days').format("L");
-            forecastIcon2.setAttribute("src", "https://openweathermap.org/img/wn/" + oneCallData.daily[2].weather[0].icon + "@2x.png");
-            forecastIcon2.setAttribute("alt", oneCallData.daily[2].weather[0].description)
-            forecastTemp2.textContent = "Temp: " + oneCallData.daily[2].temp.day;
-            forecastWind2.textContent = "Wind: " + oneCallData.daily[2].wind_speed;
-            forecastHumidity2.textContent = "Humidity: " + oneCallData.daily[2].humidity;
-            forecastDate3.textContent = moment().add(3, 'days').format("L");
-            forecastIcon3.setAttribute("src", "https://openweathermap.org/img/wn/" + oneCallData.daily[3].weather[0].icon + "@2x.png");
-            forecastIcon3.setAttribute("alt", oneCallData.daily[3].weather[0].description)
-            forecastTemp3.textContent = "Temp: " + oneCallData.daily[3].temp.day;
-            forecastWind3.textContent = "Wind: " + oneCallData.daily[3].wind_speed;
-            forecastHumidity3.textContent = "Humidity: " + oneCallData.daily[3].humidity;
-            forecastDate4.textContent = moment().add(4, 'days').format("L");
-            forecastIcon4.setAttribute("src", "https://openweathermap.org/img/wn/" + oneCallData.daily[4].weather[0].icon + "@2x.png");
-            forecastIcon4.setAttribute("alt", oneCallData.daily[4].weather[0].description)
-            forecastTemp4.textContent = "Temp: " + oneCallData.daily[4].temp.day;
-            forecastWind4.textContent = "Wind: " + oneCallData.daily[4].wind_speed;
-            forecastHumidity4.textContent = "Humidity: " + oneCallData.daily[4].humidity;
-            forecastDate5.textContent = moment().add(5, 'days').format("L");
-            forecastIcon5.setAttribute("src", "https://openweathermap.org/img/wn/" + oneCallData.daily[5].weather[0].icon + "@2x.png");
-            forecastIcon5.setAttribute("alt", oneCallData.daily[5].weather[0].description)
-            forecastTemp5.textContent = "Temp: " + oneCallData.daily[5].temp.day;
-            forecastWind5.textContent = "Wind: " + oneCallData.daily[5].wind_speed;
-            forecastHumidity5.textContent = "Humidity: " + oneCallData.daily[5].humidity;
+        if (response.status == 200){
+            return response.json()
+            .then(function(data){
+                // console.log(data)
+                // console.log(data.list[0].main)
+                // console.log(data.city.name)
+                var weatherContainer = document.querySelector("#weatherContainer")
+                weatherContainer.classList.remove("hidden");
+                cityHead.textContent = data.city.name + " " + "(" + moment().format("L") + ")";
+                cityTemp.textContent = "Temperature: " + data.list[0].main.temp;
+                cityMoist.textContent = "Humidity: " + data.list[0].main.humidity + "%";
+                cityWind.textContent = "Wind Speed: " + data.list[0].wind.speed + "MPH";
+                citySun.textContent = "UV Index: ";
+                
+                
+                var lat = data.city.coord.lat
+                var lon = data.city.coord.lon
+                var oneCallURL = "https://api.openweathermap.org/data/2.5/onecall?lat="+ lat + "&lon=" + lon + "&units=imperial&appid=" + apiKey;
+                fetch(oneCallURL)
+                .then(function(secondResponse){
+                    return secondResponse.json()
+                .then(function(oneCallData){
+                    // console.log("this is the one call data below")
+                    console.log(oneCallData)
+                    citySun.textContent = "UV Index: " + oneCallData.daily[0].uvi;
+                    uvIndexValue = Number(oneCallData.daily[0].uvi)
+                    console.log("uvIndexValue: " +uvIndexValue)
+                    if (uvIndexValue <= 3) {
+                        // console.log("the Uv Index is low")
+                        citySun.style.backgroundColor = "green";
+                    } else if (uvIndexValue >= 7) {
+                        // console.log("the UV Index is severe")
+                        citySun.style.backgroundColor = "red";
+                    } else {
+                        // console.log("the UV Index is moderate")
+                        citySun.style.backgroundColor = "yellow"
+                    }
+                    cityIcon.setAttribute("src", "https://openweathermap.org/img/wn/" + oneCallData.daily[0].weather[0].icon + "@2x.png");
+                    cityIcon.setAttribute("alt", oneCallData.daily[0].weather[0].description);
+                    forecastDate1.textContent = moment().add(1, 'days').format("L");
+                    forecastIcon1.setAttribute("src", "https://openweathermap.org/img/wn/" + oneCallData.daily[1].weather[0].icon + "@2x.png");
+                    forecastIcon1.setAttribute("alt", oneCallData.daily[1].weather[0].description)
+                    forecastTemp1.textContent = "Temp: " + oneCallData.daily[1].temp.day;
+                    forecastWind1.textContent = "Wind: " + oneCallData.daily[1].wind_speed;
+                    forecastHumidity1.textContent = "Humidity: " + oneCallData.daily[1].humidity;
+                    forecastDate2.textContent = moment().add(2, 'days').format("L");
+                    forecastIcon2.setAttribute("src", "https://openweathermap.org/img/wn/" + oneCallData.daily[2].weather[0].icon + "@2x.png");
+                    forecastIcon2.setAttribute("alt", oneCallData.daily[2].weather[0].description)
+                    forecastTemp2.textContent = "Temp: " + oneCallData.daily[2].temp.day;
+                    forecastWind2.textContent = "Wind: " + oneCallData.daily[2].wind_speed;
+                    forecastHumidity2.textContent = "Humidity: " + oneCallData.daily[2].humidity;
+                    forecastDate3.textContent = moment().add(3, 'days').format("L");
+                    forecastIcon3.setAttribute("src", "https://openweathermap.org/img/wn/" + oneCallData.daily[3].weather[0].icon + "@2x.png");
+                    forecastIcon3.setAttribute("alt", oneCallData.daily[3].weather[0].description)
+                    forecastTemp3.textContent = "Temp: " + oneCallData.daily[3].temp.day;
+                    forecastWind3.textContent = "Wind: " + oneCallData.daily[3].wind_speed;
+                    forecastHumidity3.textContent = "Humidity: " + oneCallData.daily[3].humidity;
+                    forecastDate4.textContent = moment().add(4, 'days').format("L");
+                    forecastIcon4.setAttribute("src", "https://openweathermap.org/img/wn/" + oneCallData.daily[4].weather[0].icon + "@2x.png");
+                    forecastIcon4.setAttribute("alt", oneCallData.daily[4].weather[0].description)
+                    forecastTemp4.textContent = "Temp: " + oneCallData.daily[4].temp.day;
+                    forecastWind4.textContent = "Wind: " + oneCallData.daily[4].wind_speed;
+                    forecastHumidity4.textContent = "Humidity: " + oneCallData.daily[4].humidity;
+                    forecastDate5.textContent = moment().add(5, 'days').format("L");
+                    forecastIcon5.setAttribute("src", "https://openweathermap.org/img/wn/" + oneCallData.daily[5].weather[0].icon + "@2x.png");
+                    forecastIcon5.setAttribute("alt", oneCallData.daily[5].weather[0].description)
+                    forecastTemp5.textContent = "Temp: " + oneCallData.daily[5].temp.day;
+                    forecastWind5.textContent = "Wind: " + oneCallData.daily[5].wind_speed;
+                    forecastHumidity5.textContent = "Humidity: " + oneCallData.daily[5].humidity;
+                    })
+                })
             })
-        })
+        } else {
+            alert("404 - Please input a valid city")
+        }
+        
     })
+   
+    
 }
 
 //Functions to save and retrieve data from Local Storage and create the buttons
